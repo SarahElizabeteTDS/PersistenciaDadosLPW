6b8e53720eb17488d1341ec7a2a9e2b149255e83
SarahElizabeteTDS <130590547+SarahElizabeteTDS@users.noreply.github.com>
2025-05-23 15:43:32 -0300

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,6 @@ $stm->execute();
 $livros = $stm->fetchAll();
 
 
-
 if (isset($_POST["titulo"])) 
 {
 
@@ -19,10 +18,12 @@ if (isset($_POST["titulo"]))
     $titulo = $_POST["titulo"];
     $genero = $_POST["genero"];
     $numPaginas = $_POST["paginas"];
+    $Autor = $_POST["Autor"];
 
-    $sql = "INSERT INTO livros(titulo, genero, qtd_paginas) VALUES (?,?,?)";
+    $sql = "INSERT INTO livros(titulo, genero, qtd_paginas, Autor) VALUES (?,?,?,?)";
     $stm = $con->prepare($sql);
-    $stm->execute([$titulo,$genero,$numPaginas]);
+    $stm->execute([$titulo,$genero,$numPaginas, $Autor]);
+    header("location: index.php");
 }
 ?>
 
@@ -42,22 +43,31 @@ if (isset($_POST["titulo"]))
             <th>Título</th>
             <th>Gênero</th>
             <th>NumPáginas</th>
+            <th>Autor</th>
+            <th>Excluir</th>
         </tr>
         <?php
         foreach ($livros as $l)
         {
-            print <<<TEST
-                <tr>
-                    <td>{$l['id']}</td>
-                    <td>{$l['titulo']}</td>
-                    <td>{$l['genero']}</td>
-                    <td>{$l['qtd_paginas']}</td>
-                </tr>
-            TEST;
+            $gen = "";
+            if ($l['genero'] == "D") 
+            {
+                $gen = "Drama";
+            }elseif ($l['genero'] == "F") 
+            {
+                $gen = "Ficção";
+            }elseif ($l['genero'] == "R") {
+                $gen = "Romance";
+            }else
+            {
+                $gen = "Outro";
+            }
+
+            print "<tr><td>{$l['id']}</td><td>{$l['titulo']}</td><td>{$gen}</td><td>{$l['qtd_paginas']}</td><td>{$l['Autor']}</td><td><a href='excluir.php?excluir={$l['id']}"+ 'onclick=return confirm("Confirma a exclusão?")'+">Excluir</a></td></tr>";
         } 
         ?>
     </table>
-
+    
     <h1>Formulário</h1>
 
     <form action="" method="post">
@@ -78,6 +88,10 @@ if (isset($_POST["titulo"]))
         <div style="margin-block-end: 15px;">
             <label for="paginas">Num de Páginas:</label>
             <input type="number" name="paginas" id="paginas">
+        </div>
+        <div style="margin-block-end: 15px;">
+            <label for="Autor">Autor:</label>
+            <input type="text" name="Autor" id="Autor">
         </div>
         <div style="margin-block-end: 15px;">
             <button type="submit">Enviar</button>
